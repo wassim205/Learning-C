1662ff81e147d88febdd02c24c3b85f79216070e
Wassim <wassimelmorabit7@gmail.com>
2024-09-08 13:08:54 +0100

    Pointers

--- Learning C/Pointers.c
+++ Learning C/Pointers.c
@@ -2,10 +2,15 @@
 
 int main(){
 
-    
+    int Birth_year;
+    int *pBirth = &Birth_year;
 
 
+    printf("What is your birth year ?:");
+    scanf("%d", &Birth_year);
 
+    int age = 2024 - *pBirth;
 
+    printf("you're %d years old", age);
     return 0;
 }
